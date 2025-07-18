0f5255fbfe647c4b16e9c56210cea6cc07a33c63
vinhtam205 <vinhtam306205@gmail.com>
2025-07-18 14:21:10 +0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,31 @@
+new Swiper('.swiper', { 
+    loop: true, 
+    spaceBetween: 30, 
+    slidesPerView: 3,
+
+    // Pagination bullets 
+    pagination: { 
+        el: '.swiper-pagination', 
+        clickable: true, 
+        dynamicBullets: true 
+    }, 
+
+    // Navigation arrows 
+    navigation: { 
+        nextEl: '.swiper-button-next', 
+        prevEl: '.swiper-button-prev', 
+    }, 
+
+    // Responsive breakpoints 
+    breakpoints: { 
+        0: { 
+            slidesPerView: 1 
+        }, 
+        768: { 
+            slidesPerView: 2 
+        }, 
+        1024: { 
+            slidesPerView: 3 
+        } 
+    }
+}); 
